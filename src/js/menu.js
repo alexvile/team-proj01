166a373d3ac6deaaa30a1dbabafda84467670968
Alexei Solotvinskiy <alexei20293@yahoo.com>
2022-05-16 13:13:03 +0300

    add anchors and close mobile menu logic

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,13 +2,24 @@
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
+    closeMenuList: document.querySelector('.mt-menu__navigation-list'),
     menu: document.querySelector('[data-menu]'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuList.addEventListener('click', closeMobileMenu);
 
   function toggleModal() {
     refs.menu.classList.toggle('is-open');
+  }
+  function closeMobileMenu(e) {
+    if(!e.target.classList.contains('mt-menu__navigation-link')) {
+      return
+    }
+    if(e.target.classList.contains('mt-menu__navigation-link')) {
+      refs.menu.classList.toggle('is-open');
+    }
   }
 })();
